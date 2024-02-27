c51c2aec7f1983594ebaef96960b9e7bb50c382f
unknown <teddyrbee@gmail.com>
2024-02-27 08:51:23 -0400

    cambios

--- WebParaMelvin/Models/Info_general.cs
+++ WebParaMelvin/Models/Info_general.cs
@@ -30,18 +30,18 @@ namespace WebParaMelvin.Models
         public Nullable<int> Id_Formulario_S_O { get; set; }
         public Nullable<System.DateTime> Ultima_modificacion { get; set; }
         public Nullable<int> Usuario_que_modifico { get; set; }
-        public Nullable<bool> Pre_espirometria { get; set; }
-        public Nullable<bool> Espirometria { get; set; }
-        public Nullable<bool> Audiometria { get; set; }
-        public Nullable<bool> Capacidad_fisica { get; set; }
-        public Nullable<bool> Laboratorio { get; set; }
-        public Nullable<bool> Radiografia { get; set; }
-        public Nullable<bool> Hemografia { get; set; }
-        public Nullable<bool> EKG { get; set; }
-        public Nullable<bool> Examen_visual { get; set; }
-        public Nullable<bool> Bateria_de_un_mes { get; set; }
-        public Nullable<bool> Bateria_Completa { get; set; }
-        public Nullable<bool> Historia_clinica_y_examen_fisico { get; set; }
+        public bool Pre_espirometria { get; set; }
+        public bool Espirometria { get; set; }
+        public bool Audiometria { get; set; }
+        public bool Capacidad_fisica { get; set; }
+        public bool Laboratorio { get; set; }
+        public bool Radiografia { get; set; }
+        public bool Hemografia { get; set; }
+        public bool EKG { get; set; }
+        public bool Examen_visual { get; set; }
+        public bool Bateria_de_un_mes { get; set; }
+        public bool Bateria_Completa { get; set; }
+        public bool Historia_clinica_y_examen_fisico { get; set; }
         public bool Consentimiento_informado { get; set; }
         public string Estado { get; set; }
     
