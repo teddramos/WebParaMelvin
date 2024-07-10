250561295a0066013109a6b74cc3eb5832c603e5
unknown <teodosio.rodriguez@utreee.com>
2024-07-10 15:41:36 -0400

    cambios

--- WebParaMelvin/Models/Info_general.cs
+++ WebParaMelvin/Models/Info_general.cs
@@ -45,6 +45,7 @@ namespace WebParaMelvin.Models
         public bool Consentimiento_informado { get; set; }
         public string Estado { get; set; }
         public bool EpWorth { get; set; }
+        public bool Apnea { get; set; }
     
         public virtual Formulario_S_O Formulario_S_O { get; set; }
     }
